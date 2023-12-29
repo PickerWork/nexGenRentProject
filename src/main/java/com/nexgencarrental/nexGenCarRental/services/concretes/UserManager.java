0dfdbf97fc96f17ae5b4a1e2c0a52100bd9b7acc
PickerWork <pickerwork@gmail.com>
2023-12-29 08:19:22 +0300

    UserManager Updated

--- src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/UserManager.java
+++ src/main/java/com/nexgencarrental/nexGenCarRental/services/concretes/UserManager.java
@@ -17,9 +17,6 @@ public class UserManager extends BaseManager<User, UserRepository, GetUserRespon
     private UserBusinessRulesService userBusinessRulesService;
     private ModelMapperService modelMapperService;
 
-    public UserManager(UserRepository repository, ModelMapperService modelMapperService, UserBusinessRulesService userBusinessRulesService) {
-        super(repository, modelMapperService, GetUserResponse.class, GetUserListResponse.class, User.class, AddUserRequest.class, UpdateUserRequest.class);
-    }
 
 
     @Override
